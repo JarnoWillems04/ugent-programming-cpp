412472d69af3a609edf319807b6184dbc18bb27b
Jarno Willems <jarnowillems001@gmail.com>
2025-11-17 19:00:10 +0100

    Added oef 1-8 + made oef 1

---
+++ labo0/oefening1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+    std::cout << "Hello world!\n";
+    for (int i = 10; i > 0; i--) {
+        std::cout << i <<" ";
+    }
+    std::cout << "\nSTART\n";
+}
