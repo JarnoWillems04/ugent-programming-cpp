e9b5489ec549250b59ceb98752c4cc54860c8428
Jarno Willems <jarnowillems001@gmail.com>
2025-12-12 10:35:13 +0100

    Oefening 20 gemaakt

--- labo5-overerving/oefening20.cpp
+++ labo5-overerving/oefening20.cpp
@@ -9,7 +9,7 @@ public:
     // rechthoek(int hoogte = 1, int breedte = 1) : hoogte(hoogte), breedte(breedte) {}
     int omtrek() const;
     int oppervlakte() const;
-    void print(ostream &) const;
+    virtual void print(ostream &) const;
     friend ostream& operator<<(ostream &, const rechthoek &);
 protected:
     int hoogte;
@@ -41,10 +41,10 @@ private:
 public:
     gekleurde_rechthoek(int hoogte = 1, int breedte = 1, const string & kleur = "onbekend")
          : rechthoek(hoogte, breedte), kleur(kleur) {}
-    void print(ostream & out);
+    void print(ostream & out) const override;
 };
 
-void gekleurde_rechthoek::print(ostream & out) {
+void gekleurde_rechthoek::print(ostream & out) const{
     rechthoek::print(out);
     out << "  kleur: " << kleur << endl;
 }
@@ -54,31 +54,26 @@ void gekleurde_rechthoek::print(ostream & out) {
 class vierkant : public rechthoek {
 public:
     vierkant(int z = 1) : rechthoek(z, z) {}
-    void print(ostream &out);
+    void print(ostream &out) const override;
 };
 
-void vierkant::print(ostream & out) {
+void vierkant::print(ostream & out) const {
     out << "Vierkant: " << "zijde: " << hoogte << endl ;
 }
 
 /*************** main ***************/
 
 int main() {
-    rechthoek r2(4,6);
-    gekleurde_rechthoek gr1;
-    gekleurde_rechthoek gr3(6,9,"rood");
-    vierkant v2(10);
+    vector<unique_ptr<rechthoek>> v;
 
-    vector<rechthoek> v;
-    v.push_back(r2);
-    v.push_back(gr1);
-    v.push_back(gr3);
-    v.push_back(v2);
-
+    v.push_back(make_unique<rechthoek>(4, 6));
+    v.push_back(make_unique<gekleurde_rechthoek>());
+    v.push_back(make_unique<gekleurde_rechthoek>(6, 9, "rood"));
+    v.push_back(make_unique<vierkant>(10));
     for (int i=0 ; i < v.size() ; i++) {
-        cout << v[i];
-        cout << "  oppervlakte: " << v[i].oppervlakte() << endl
-             << "  omtrek: " << v[i].omtrek() << endl;
+        cout << *v[i];
+        cout << "  oppervlakte: " << v[i]->oppervlakte() << endl
+             << "  omtrek: " << v[i]->omtrek() << endl;
     }
     return 0;
 }
